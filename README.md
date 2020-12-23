2dac1aed42317f6cb317e16aa8f882a300224a21
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-12-23 15:17:02 +0530

    Week-1 Implementation and STL

---
+++ README.md
@@ -0,0 +1,9 @@
+# Competitve Programming Practice
+
+_Codeforces Group: https://codeforces.com/group/Upc8dBSSKf_
+
+|  Week  |        Topic         |          Resources          |
+| :----: | :------------------: | :-------------------------: |
+| Week 1 | Implementation & STL | http://bit.ly/GNCSGI-W1-STL |
+| Week 2 |    Number Theory     |   http://bit.ly/GNCSGI-W2   |
+|        |                      |                             |
---
+++ Week 1 - Implementation And STL/DZYLovesDASH.cpp
@@ -0,0 +1,91 @@
+/*
+ * Author : Siddhant Khare
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define ll long long
+#define ld long double
+#define arr array
+#define si(t) scanf("%d", &t)
+#define sl(t) scanf("%ld", &t)
+#define sll(t) scanf("%lld", &t)
+#define sc(t) scanf("%c", &t)
+#define ss(t) scanf("%s", &t)
+#define sf(t) scanf("%f", &t)
+#define slf(t) scanf("%lf", &t)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%ld\n", x)
+#define pll(x) printf("%lld\n", x)
+#define pf(x) printf("%f")
+#define sz(x) (int)(x).size()
+#define mem(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i = j; i < k; i += in)
+#define RFOR(i, j, k, in) for (int i = j; i >= k; i -= in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(x) x.begin(), x.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert(B <= A && A <= C)
+#define sortall(x) sort(all(x))
+#define mp make_pair
+#define pb push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi = acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+
+/**************************************/
+
+/******** User-defined Function *******/
+void solve()
+{
+    //    ll a, b, c, d;
+    //  scanf("%lld%lld%lld%lld", &a, &b, &c, &d);
+    // Algorithm
+
+    // Output
+    // pll(ans) // long long print
+    int p, n;
+    cin >> p >> n;
+    ll a[n];
+    int b[p] = { 0 };
+    int k = -1;
+    REP(i, n)
+    {
+        cin >> a[i];
+        b[a[i] % p]++;
+        if (b[a[i] % p] == 2 && k == -1)
+            k = i + 1;
+    }
+    cout << k << endl;
+}
+
+/**************************************/
+
+/********** Main()  function **********/
+int main()
+{
+
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+
+    int T = 1;
+    //  scanf("%lld", &T);
+    while (T--) {
+        solve();
+    }
+}
+/********  Main() Ends Here *************/
